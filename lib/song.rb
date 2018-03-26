405b1700e90b783398a1b60355801b76e58560f4
Alexander Spanos <alexisspa9@gmail.com>
2018-03-26 12:24:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
 
 def initialize(name)
   @name = name
-  
+
 end
 
   def self.new_by_filename(filename)
@@ -12,6 +12,9 @@ end
     new_song.artist_name = artist
     new_song
   end
-
+  def artist_name=(name)
+      self.artist = Artist.find_or_create_by_name(name)
+      artist.add_song(self)
+    end
 
 end
